60eaa6d0a1fa0894a3340f8c24fb2635553ce535
Garrett Dale Asper <garrettasper@vt.edu>
2024-07-05 23:39:42 -0400

    large commit of overarching structure changes and organizational improvements

--- call_plotTheBasics.m
+++ call_plotTheBasics.m
@@ -1,38 +1,48 @@
-
-% Description: This script allows the user to properly call the 
-%   plotTheBasics function, which facilitates the visualization of basic 
-%   flight data from a .mat file. By setting up input parameters such as the 
-%   file name, path, selection type, and optional data trimming, users can 
-%   easily generate visualizations either interactively or programmatically. 
+% Example Call 'plotTheBasics.m' Function Script
+%
+% DESCRIPTION: 
+%   This script allows the user to properly call the plotTheBasics function,
+%   which facilitates the visualization of basic flight data from a .mat file.
+%   By setting up input parameters such as the file name, path, selection type,
+%   and optional data trimming, users can easily generate visualizations 
+%   either interactively or programmatically.
+%
+% WRITTEN BY:
+%   Garrett D. Asper
+%   Virginia Tech
+%   Email: garrettasper@vt.edu
+%
+% HISTORY:
+%   03 JUL 2024 - Created and debugged, GDA
+%
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND
+%
+%       1 - Plots 4 plots, includes 1. NED, 2. uvw, 3. phi theta psi, 4.
+%       pqr
+%       2 - phi theta r vs cmd
+%       3 - Alt vs Alt_sp
+%       4 - RPM vs RPM_cmd
+%       5 - VV vs VV_sp
 
 clear; clc;
 
 % Define the name of the .mat file containing the flight data
-mat_name = '20240327_Flight2.mat';
+mat_name = '19_40_36.mat';
 
 % Define the path to the directory containing the .mat file
-mat_path = 'C:\Users\gurt1\OneDrive - Virginia Tech\Documents\NSL Research\Flight Logs - HATCH\20240327\';
+mat_path = 'C:\Users\gasper\OneDrive - NASA\Documents\IMPACT Vehicle\Aircraft Logbook\20240702\';
 
-% Specify the type of plot to visualize:
-%   1 - NED, uvw, phi theta psi, pqr
-%   2 - NED, uvw
-%   3 - phi theta psi, pqr
-%   4 - NED
-%   5 - uvw
-%   6 - phi theta psi
-%   7 - pqr
-%   8 - phi theta r vs cmd
-%   9 - D vs D_cmd
-%   10 - RPM vs RPM_cmd
-%   11 - Plots 4 plots, includes 1. NED, 2. uvw, 3. phi theta psi, 4. pqr
-
-selection = 8;
+% Choose the selection number based on the desired plot
+selection = 4;
 
 % Specify the start time in seconds for data trimming (optional)
 trim_start = 1;
 
 % Specify the end time in seconds for data trimming (optional)
-trim_end = 0;
+trim_end = 10;
+
+% Select if the plot will be exported to a PDF
+export = 'false'; % either 'true' or 'false'
 
 % Call the function to visualize the flight data based on the provided inputs
-plotTheBasics(mat_name, mat_path, selection, trim_start, trim_end);
+plotTheBasics(mat_name, mat_path, selection, trim_start, trim_end, export);
